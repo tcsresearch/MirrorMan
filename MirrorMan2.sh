a559993fb8d253f125c5fbc5049d782eb91d2416
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2024-10-25 23:21:56 -0500

    Update MirrorMan2.sh

--- MirrorMan2.sh
+++ MirrorMan2.sh
@@ -1,19 +1,25 @@
 ### Source config.ini ###
+function SourceConfig() {
 # TODO: Add Sanity Check.
-echo "Sourcing config/config.ini..."
-source config/config.ini
-echo "Successfully sourced config.ini"
+	echo "Sourcing config/config.ini..."
+	source config/config.ini
+	echo "Successfully sourced config.ini"
+}
 
 ### Enable Cecho ###
+function EnableCecho() {
 # TODO: Add Sanity Check.
-source $FunctionsDir/NewCecho.bfunc
-cecho yellow "Cecho Enabled."
+	source $FunctionsDir/NewCecho.bfunc
+	cecho yellow "Cecho Enabled."
+}
 
 # TODO: Add Sanity Check.
-for repo in `cat $ConfigDir/repos.list`; do
-	cecho yellow "Updating repo $repo..."
-	cd $repo
-	sh Update.sh
-	cd -
-	cecho yellow "Repo $repo Updated."
-done
+function DoSync() {
+	for repo in `cat $ConfigDir/repos.list`; do
+		cecho yellow "Updating repo $repo..."
+		cd $repo
+		sh Update.sh
+		cd -
+		cecho yellow "Repo $repo Updated."
+	done
+}
