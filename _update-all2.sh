c43f95abe7859d68c74443b3f8a0391f47985b10
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-04-30 02:35:19 -0500

    Refactor repo update script for better readability

--- _update-all2.sh
+++ _update-all2.sh
@@ -1,15 +1,17 @@
+#!/usr/bin/env bash
+
 ### Enable Cecho ###
 # TODO: Add Sanity Check.
-source ./NewCecho.bfunc
+source "./NewCecho.bfunc"
 cecho blue "Cecho Enabled."
 echo " "
 
-for repo in `cat repos.list`; do
+for repo in $(cat repos.list); do
 	cecho yellow "##########---------- Updating Repo: $repo... ----------##########"
 	echo " "
-	cd $repo > /dev/null
+	cd "$repo" > /dev/null || return 1
 	sh Update.sh
-	cd - > /dev/null
+	cd ... > /dev/null || return 1
 	echo " "
 	cecho blue "Repo $repo Updated."
 	echo " "
