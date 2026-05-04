0c33bde3cf18b4040e9132160a4a4593dd24d0e1
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-04 13:16:46 -0500

    Add files via upload

---
+++ Update.sh
@@ -0,0 +1,14 @@
+## Define Variables ##
+
+prog="reposync"
+args=" -n -m --delete"
+excludes="*-kmodsrc-*"
+
+# reposync -n -m --delete --repoid=kernel-vanilla:fedora
+# reposync -n -m --delete --repoid=kernel-vanilla:stable
+
+$prog $args --repoid=rpmfusion-free
+$prog $args --repoid=rpmfusion-free-updates
+
+$prog $args --repoid=rpmfusion-nonfree
+$prog $args --repoid=rpmfusion-nonfree-updates
