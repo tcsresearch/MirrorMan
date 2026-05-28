f6ab88ee778f10ce3f94cc1c1bbdb5df6796476d
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-28 12:28:49 -0500

    Add quotes around variables in Update.sh
    Updated script to use quotes around variables for repo IDs.

--- Update.sh
+++ Update.sh
@@ -8,8 +8,8 @@ excludes="*-kmodsrc-*"
 # reposync -n -m --delete --repoid=kernel-vanilla:fedora
 # reposync -n -m --delete --repoid=kernel-vanilla:stable
 
-$prog $args --repoid=rpmfusion-free
-$prog $args --repoid=rpmfusion-free-updates
+"$prog" "$args" --repoid=rpmfusion-free
+"$prog" "$args" --repoid=rpmfusion-free-updates
 
-$prog $args --repoid=rpmfusion-nonfree
-$prog $args --repoid=rpmfusion-nonfree-updates
+"$prog" "$args" --repoid=rpmfusion-nonfree
+"$prog" "$args" --repoid=rpmfusion-nonfree-updates
