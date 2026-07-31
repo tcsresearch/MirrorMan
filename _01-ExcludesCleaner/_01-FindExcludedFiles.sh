d29823f2f0bb8b579ac85fac19c6c762d4a749e2
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-07-31 12:48:06 -0500

    Fix find command to use current directory

--- _01-ExcludesCleaner/_01-FindExcludedFiles.sh
+++ _01-ExcludesCleaner/_01-FindExcludedFiles.sh
@@ -1,2 +1,3 @@
 #!/bin/env bash
-find | grep -f excludes-filtered2.list | tee ExcludedFiles.list
+## find | grep -f excludes-filtered2.list | tee ExcludedFiles.list
+find . | grep -f excludes-filtered2.list | tee ExcludedFiles.list
